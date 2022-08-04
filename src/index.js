df03178d33cf5969f11c3c32d734a94b93b2a3db
Paulo <pc5783346@gmail.com>
2022-08-03 23:35:26 -0300

    doc: file creation

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.listen(process.env.PORT, ()=>
+    console.log("Server is listening on port.")
+);
