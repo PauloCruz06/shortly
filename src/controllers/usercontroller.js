cdcbcb3376814f8edf9e72a66986e6707d71c19d
Paulo <pc5783346@gmail.com>
2022-08-05 13:58:43 -0300

    feat: it's now possible to delete user's short urls

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -27,3 +27,33 @@ export async function postUrl(req, res) {
         res.status(500).send(e);
     }
 }
+
+export async function deleteUrl(req, res) {
+    try {
+        const { userId } = res.locals;
+        const { id } = req.params;
+
+        if(!id) return res.sendStatus(422);
+
+        const { rows: linkList } = await connection.query(`
+            SELECT id, "userId" FROM links
+            WHERE id = $1
+        `, [id]);
+
+        if(linkList <= 0) return res.sendStatus(404);
+        
+        if(linkList.some(link =>
+            link.userId !== userId
+        )) return res.sendStatus(401);
+
+        await connection.query(`
+            DELETE FROM links
+            WHERE id = $1
+        `, [id]);
+
+        res.sendStatus(204);
+    } catch(e) {
+        res.status(500).send(e);
+    }
+
+}
--- src/routes/userrouter.js
+++ src/routes/userrouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import userTokenValidation from "../middlewares/tokenValidation.js";
-import { postUrl } from "../controllers/usercontroller.js";
+import { postUrl, deleteUrl } from "../controllers/usercontroller.js";
 
 const userrouter = Router();
 
 userrouter.use(userTokenValidation);
 userrouter.post('/urls/shorten', postUrl);
+userrouter.delete('/urls/:id', deleteUrl);
 
 export default userrouter;
